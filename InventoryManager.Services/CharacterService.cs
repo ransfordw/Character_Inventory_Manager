8b9386a3b69313fd59fc610271cf4e6bdacba0d1
Ransford Walker <ransfordw@gmail.com>
2018-08-16 11:36:47 -0400

    modifiedServices

---
+++ InventoryManager.Services/CharacterService.cs
@@ -0,0 +1,60 @@
+﻿using InventoryManager.Data;
+using InventoryManager.Models;
+using InventoryMangager.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager.Services
+{
+    public class CharacterService
+    {
+        private readonly Guid _userId;
+        public CharacterService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateCharacter(CharacterCreate model)
+        {
+            var entity = new Character()
+            {
+                OwnerID = _userId,
+                CharacterName = model.CharacterName,
+                CharacterClass = model.CharacterClass,
+                CharacterRace = model.CharacterRace,
+
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Characters.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CharacterListItem> GetCharacters()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Characters
+                        .Where(e => e.OwnerID == _userId)
+                        .Select(
+                            e =>
+                                new CharacterListItem
+                                {
+                                    CharacterID = e.CharacterID,
+                                    CharacterName = e.CharacterName,
+                                    CharacterClass = e.CharacterClass,
+                                    CharacterRace = e.CharacterRace,
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
