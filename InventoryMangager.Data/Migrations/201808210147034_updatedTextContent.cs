1669587c838cdf82a47c2f336114e7ab5cd96796
Ransford Walker <ransfordw@gmail.com>
2018-08-20 21:48:33 -0400

    updatedTextContent

---
+++ InventoryMangager.Data/Migrations/201808210147034_updatedTextContent.cs
@@ -0,0 +1,16 @@
+namespace InventoryMangager.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedTextContent : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
